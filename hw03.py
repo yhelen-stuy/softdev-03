6e9e9fdcd2362c3f678ce7e6f1f788a8e9f67748
Helen Ye <00yhelen@gmail.com>
2017-09-15 20:32:27 -0400

    Add comments

--- hw03.py
+++ hw03.py
@@ -1,13 +1,20 @@
 import random
 def read_occupations():
+    # Read the file
+    # 'U' is the universal newline (in case the csv file was made in Windows)
     source = open('occupations.csv', 'rU')
+    # Strip of newlines at the beginning of end of file before splitting/parsing
     lines = source.read().strip('\n').split('\n')
     source.close()
     csv_dict = {}
+    # Remove the title line
     lines.pop(0)
     for line in lines:
+        # Check if the job title includes commas and if so account for it
         if line[0:1] is '"':
+            # Find the second quote
             end_quote = line[1:].find('"') + 1
+            # Define the list accordingly
             fields = [line[1:end_quote], line[end_quote+2:]]
         else:
             fields = line.split(',')
@@ -15,7 +22,6 @@ def read_occupations():
     return csv_dict
 
 def random_profession(professions):
-    
 
 occ = read_occupations()
 for x in occ:
